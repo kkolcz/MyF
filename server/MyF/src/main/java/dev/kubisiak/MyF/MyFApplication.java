7fc39800ee1623a0a53ce2a3313eecaed8ac805f
MarvelSas <marcinkk543@gmail.com>
2025-02-06 18:19:41 +0100

    -fix

---
+++ server/MyF/src/main/java/dev/kubisiak/MyF/MyFApplication.java
@@ -0,0 +1,13 @@
+package dev.kubisiak.MyF;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFApplication.class, args);
+	}
+
+}
---
+++ server/MyF/src/test/java/dev/kubisiak/MyF/MyFApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kubisiak.MyF;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
