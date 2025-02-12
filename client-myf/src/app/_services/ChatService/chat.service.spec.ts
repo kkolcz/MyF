d2471bc934c20cf0735e9b8bd44e8f697bf6e397
kkolcz <k.kolcz@outlook.com>
2025-02-12 12:10:26 +0100

    fix error

--- client-myf/src/app/_services/ChatService/chat.service.spec.ts
+++ client-myf/src/app/_services/ChatService/chat.service.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 
-import { ChatService } from './ChatService/chat.service';
+import { ChatService } from './chat.service';
 
 describe('ChatService', () => {
   let service: ChatService;
