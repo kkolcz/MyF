698a2f1683582b99062a3e81f598f1cb10c49fdf
MarvelSas <marcinkk543@gmail.com>
2025-02-23 15:52:17 +0100

    -Add more precise user endpoint

--- server/MyF/src/main/java/dev/kubisiak/MyF/user/UserController.java
+++ server/MyF/src/main/java/dev/kubisiak/MyF/user/UserController.java
@@ -19,7 +19,7 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping
+    @GetMapping("/users")
     public ResponseEntity<List<UserResponse>> getAllUsers(Authentication authentication){
         return ResponseEntity.ok(userService.getAllUsersExceptSelf(authentication));
     }
