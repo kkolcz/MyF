1666a924aeb837004f3b79ae35be8fe06aaa85e2
MarvelSas <marcinkk543@gmail.com>
2025-02-26 19:32:10 +0100

    -update logic of websocket messages.

--- server/MyF/src/main/java/dev/kubisiak/MyF/message/MessageService.java
+++ server/MyF/src/main/java/dev/kubisiak/MyF/message/MessageService.java
@@ -43,7 +43,8 @@ public class MessageService {
 
         chat.getUsers().stream().forEach((user) -> {
             log.info("Sending message to user {} with content {}", user.getId(), wsMessage.getContent());
-            simpMessagingTemplate.convertAndSendToUser(user.getId(), "/messages", wsMessage);
+//            simpMessagingTemplate.convertAndSendToUser(user.getId(), "/messages", wsMessage);
+            simpMessagingTemplate.convertAndSend("/topic/messages/"+user.getId(), wsMessage);
         });
 
 
