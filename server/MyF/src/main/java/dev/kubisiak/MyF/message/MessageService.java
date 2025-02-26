49c1fd588ade12cf2ec8ae3aecace5d17012c8e8
MarvelSas <marcinkk543@gmail.com>
2025-02-26 17:54:00 +0100

    -update logic of websocket messages.

--- server/MyF/src/main/java/dev/kubisiak/MyF/message/MessageService.java
+++ server/MyF/src/main/java/dev/kubisiak/MyF/message/MessageService.java
@@ -3,20 +3,24 @@ package dev.kubisiak.MyF.message;
 import dev.kubisiak.MyF.chat.Chat;
 import dev.kubisiak.MyF.chat.ChatRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class MessageService {
 
     private final ChatRepository chatRepository;
     private final MessageRepository messageRepository;
     private final SimpMessagingTemplate simpMessagingTemplate;
 
+    public void saveMessage(MessageRequest messageRequest) {
 
-    public void saveMessage(MessageRequest messageRequest, Authentication authentication) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         Chat chat = chatRepository.findById(messageRequest.getChatId())
                 .orElseThrow(() -> new RuntimeException("Chat not found"));
@@ -37,7 +41,8 @@ public class MessageService {
 
 
         chat.getUsers().stream().forEach((user) -> {
-            simpMessagingTemplate.convertAndSendToUser(user.getId(), "/queue/messages", wsMessage);
+            log.info("Sending message to user {} with content {}", user.getId(), wsMessage.getContent());
+            simpMessagingTemplate.convertAndSendToUser(user.getId(), "/messages", wsMessage);
         });
 
 
