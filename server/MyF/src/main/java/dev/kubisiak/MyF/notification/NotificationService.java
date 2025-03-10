6a6370dd9bdf59102cb27c607b463523417f7760
MarvelSas <marcinkk543@gmail.com>
2025-03-10 19:33:42 +0100

    -update logic of websocket messages.

--- server/MyF/src/main/java/dev/kubisiak/MyF/notification/NotificationService.java
+++ server/MyF/src/main/java/dev/kubisiak/MyF/notification/NotificationService.java
@@ -1,7 +1,9 @@
 package dev.kubisiak.MyF.notification;
 
 import dev.kubisiak.MyF.chat.Chat;
+import dev.kubisiak.MyF.chat.ChatMapper;
 import dev.kubisiak.MyF.invitation.Invitation;
+import dev.kubisiak.MyF.invitation.InvitationMapper;
 import dev.kubisiak.MyF.user.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,8 @@ import org.springframework.stereotype.Service;
 public class NotificationService {
 
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private final InvitationMapper invitationMapper;
+    private final ChatMapper chatMapper;
 
     public void sendNotification(String userId, Notification notification){
         log.info("Sending WS notification to {} with payload {}",userId,notification);
@@ -26,7 +30,7 @@ public class NotificationService {
             Notification notification = Notification.builder()
                         .content("You have been added to a new chat with " + chatCreator.getFirstName() + " " + chatCreator.getLastName())
                         .type(NotificationType.ADD_CHAT)
-                        .payload(chat)
+                        .payload(chatMapper.mapToChatResponse(chat,chatCreator))
                         .build();
             simpMessagingTemplate.convertAndSendToUser(chatReceiver.getId(),"/notification",notification);
     }
@@ -36,7 +40,7 @@ public class NotificationService {
         Notification notification = Notification.builder()
                 .content("User " + invitation.getSender().getFirstName() + " " + invitation.getSender().getLastName() + " sent you an invitation")
                 .type(NotificationType.SENT_INVITATION)
-                .payload(invitation)
+                .payload(invitationMapper.mapToInvitationResponse(invitation))
                 .build();
 
         simpMessagingTemplate.convertAndSendToUser(invitationReceiver.getId(),"/notification",notification);
@@ -49,7 +53,7 @@ public class NotificationService {
         Notification notification = Notification.builder()
                 .content("User " + invitation.getRecipient().getFirstName() + " " + invitation.getRecipient().getLastName() + " accepted your invitation")
                 .type(NotificationType.ACCEPTED_INVITATION)
-                .payload(invitation)
+                .payload(invitationMapper.mapToInvitationResponse(invitation))
                 .build();
         simpMessagingTemplate.convertAndSendToUser(invitationSender.getId(),"/notification",notification);
     }
